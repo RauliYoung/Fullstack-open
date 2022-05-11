5247834ff80593e8d1675e141fd34f7044831fb8
RauliYoung <tristan.ellenberg@gmail.com>
2022-05-11 21:40:24 +0300

    maiden tiedot

---
+++ Osa2/maiden_tiedot/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//filternames ei toimi koska ei muka saa oikeata listaa mapattavaksi.
+
+const FilterNames = (props) => {
+  const shown = props.shown;
+  const names = props.countries;
+  const filtered = props.filtered;
+  if (shown) {
+    return (
+      <div>
+        {names.map((c) => (
+          <p key={c.latlng}> {c.name.common}</p>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {filtered.map((c) => (
+          <p key={c.latlng}> {c.name.common}</p>
+        ))}
+      </div>
+    );
+  }
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [shown, setShown] = useState(false);
+
+  const onChangeHandler = (event) => {
+    nameIsInList(event);
+    event.preventDefault();
+    setFilter(event.target.value);
+  };
+  const nameIsInList = (event) => {
+    setShown(false);
+    let name = event.target.value;
+    setFilter(countries.filter((p) => p.name.common.includes(name)));
+    if (!name) {
+      setShown(true);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Fetching info...");
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      console.log("Loaded countries..");
+      setCountries(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <form>
+        Find Countries: <input onChange={onChangeHandler}></input>
+      </form>
+      <div>
+        <FilterNames countries={countries} filtered={filter} shown={shown} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
