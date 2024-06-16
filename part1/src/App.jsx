1574d6ed9341aeeae22c8411d7a23330a44fb874
RauliYoung <tristan.ellenberg@gmail.com>
2024-06-16 20:40:11 +0300

    new vite exercices

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>{props.course}</h1>
   )
 }
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = ({courses}) => {
+  return (
+    <div>
+      {courses.map((course, i) => <Part key={i} part={course.name} exercises={course.exercises}/> )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises.map((exercise) => exercise).reduce((a,b) => a+b)}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return (
+    <div>
+      <Header course={course} />
+      <Content courses={parts}/>
+     <Total exercises={parts.map((part) => part.exercises)}/>
+    </div>
+  )
+}
+
 
 export default App
